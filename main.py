422d898ebafd7ddd5e49ada4216cb68a4975a87a
Aniket Rawate <97780596+aniketrawate@users.noreply.github.com>
2025-08-13 09:02:03 +0000

    test meme site is created

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+# from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import requests
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "Hello, World!"
+def home():
+  return render_template('index.html')
+
+@app.route("/get_meme")
+def get_meme():
+    response = requests.get("https://meme-api.com/gimme")
+    data = response.json()
+    return jsonify({
+        "title": data["title"],
+        "url": data["url"]
+    })
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug = True)
